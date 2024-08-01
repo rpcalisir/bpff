805eed9b633c73d435f24d4985fc762c8487ef40
rpcalisir <rpcalisir@gmail.com>
2024-08-01 20:34:05 +0300

    Signed in user controller is implemented.

--- BalkanPanoramaFimlFestival/Controllers/SignedInUserController.cs
+++ BalkanPanoramaFimlFestival/Controllers/SignedInUserController.cs
@@ -1,9 +1,12 @@
 ﻿using BalkanPanoramaFimlFestival.Models.Account;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BalkanPanoramaFimlFestival.Controllers
 {
+    // Only SignedIn User can use this Controller and its methods
+    [Authorize]
     public class SignedInUserController : Controller
     {
         private readonly SignInManager<RegisteredUser> _signInManager;
@@ -11,6 +14,13 @@ namespace BalkanPanoramaFimlFestival.Controllers
         public SignedInUserController(SignInManager<RegisteredUser> signInManager)
         {
             _signInManager = signInManager;
+        }
+
+        // In case a user wants to go to this page, 
+        // it will be directed to LoginPath page which is configured on ConfigureApplicationCookie in Program.cs
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task<IActionResult> LogOut()
