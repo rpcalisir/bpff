9e01a7bded6807768e53d3dd6cca228169bc7f57
rpcalisir <rpcalisir@gmail.com>
2024-09-18 18:25:30 +0300

    Static pages implementation first phase

---
+++ BalkanPanoramaFimlFestival/Controllers/StaticPagesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BalkanPanoramaFilmFestival.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdvisoryBoard()
+        {
+            return View();
+        }
+    }
+}
